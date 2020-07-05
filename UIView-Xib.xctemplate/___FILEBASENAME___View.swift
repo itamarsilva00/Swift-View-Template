56d16a2bc7472db6978b5a48edf7220ee468f7b9
Itamar Silva <itamarsilva@ioasys.com.br>
2020-07-05 12:07:37 -0300

    create template

---
+++ UIView-Xib.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,15 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___VARIABLE_sceneName___View: ___VARIABLE_subclassesFrom___ {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
